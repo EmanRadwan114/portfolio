6b42aeea799d8e4e9b3bd18199f8bd801ce6a313
Eman Radwan <emanalaa11499@gmail.com>
2025-12-17 20:03:54 +0200

    setup project structure

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <button className="bg-primary text-white p-2 rounded">hello react</button>
+    </div>
+  );
+}
+
+export default App;
